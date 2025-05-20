f1373a07b7caed93347a753d3d85f33b9b0cbf19
Projects Mastery <projectsmastery82@gmail.com>
2025-05-20 07:59:15 +0000

    Add the hearth

---
+++ src/extension.ts
@@ -0,0 +1,7 @@
+// extension.ts
+import * as vscode from 'vscode';
+// import { SkyWebViewProvider } from './webview/SkyWebViewProvider';
+import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold, GenerativeModel, ChatSession } from '@google/generative-ai';
+
+// --- Configuration & State ---
+
